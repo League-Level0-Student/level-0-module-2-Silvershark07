51fbbaed916625ad668d347032c085fa41f4ddc5
audreyizadi <league@WorthyMini-5.attlocal.net>
2018-12-18 19:56:24 -0800

    Dierolland stuff
    ufdjg

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -12,12 +12,23 @@ public class Validation {
 		
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
+		int randomNumber;
+		for(int i =0; i < 10; i++){
+		randomNumber= randomMaker.nextInt(2);
 		
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+         if(randomNumber==1){
+        	 System.out.println("You are cow");
+        	 
+         }
+         else if(randomNumber==0){
+        	 System.out.println("You are a big ball");
+        	 
+        
+         }
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
---
+++ src/random/lottonumbers.java
@@ -0,0 +1,24 @@
+package random;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class lottonumbers {
+	
+		public static void main(String[] args) {
+			
+			Random randomMaker = new Random();
+		 JPanel panel = new JPanel();
+			int randomNumber0= randomMaker.nextInt(5);
+			int randomNumber1= randomMaker.nextInt(5);
+			int randomNumber2= randomMaker.nextInt(5);
+			int randomNumber3= randomMaker.nextInt(5);
+			int randomNumber4= randomMaker.nextInt(5);
+			panel.
+		
+	        
+	         
+		}
+}
